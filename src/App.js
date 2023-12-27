4c324ca0243d576841f6ae096f9b22f023176512
mihneabatala <mihneabatala@yahoo.com>
2023-12-27 16:05:30 +0200

    implemented crud operations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import { useEffect, useState } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
+import apiReqest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -32,25 +33,51 @@ function App() {
       fetchItems();
     },2000)
     
-    
   },[])
 
-  const addItem =(item) =>{
+  const addItem = async (item) =>{
     const id = items.length ? items[items.length -1].id + 1 : 1;
     const myNewItem = {id,checked: false,item};
     const listItems =[...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiReqest(API_URL,postOptions);
+    if(result) setFetchError(result);
   }
 
-  const handleCheck = (id) =>{  
+  const handleCheck = async (id) =>{  
     const listItems = items.map((item) => item.id === id ? {...item,
     checked: !item.checked } : item );
     setItems(listItems);
+
+    const myItem = listItems.filter( (item) => item.id ===id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body : JSON.stringify({checked: myItem[0].checked})
+    }
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiReqest(reqUrl,updateOptions);
+    if(result) setFetchError(result)
   }
   
-  const handleDelete = (id) =>{
+  const handleDelete = async (id) =>{
     const listItems = items.filter((item) => item.id !==id)
     setItems(listItems);
+
+    const deleteOptions = {method : 'DELETE'};
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiReqest(reqUrl,deleteOptions);
+    if(result) setFetchError(result)
   }
 
   const handleSubmit = (e) =>{
---
+++ src/apiRequest.js
@@ -0,0 +1,12 @@
+const apiReqest = async (url = '', optionsObj = null ,errMsg = null) =>{
+    try{
+        const response = await fetch(url, optionsObj);
+        if(!response.ok) throw new Error ('Please reload the app')
+    } catch(err){
+        errMsg = err.message;
+    } finally {
+        return errMsg;
+    }
+}
+
+export default apiReqest;
